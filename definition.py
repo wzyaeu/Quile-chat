ef956e61a1b83734a6e224215d967cfa267e948b
wzyaeu <wzyaeu@163.com>
2025-10-18 23:13:12 +0800

    数据库位置变更，适配新打包程序

--- definition.py
+++ definition.py
@@ -20,9 +20,9 @@ def flask_init():
     app = Flask(__name__)
     VERSION = 'v0.1.1'
 
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(BASE_DIR, 'data.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    db_path = os.path.join(os.getcwd(), 'data.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path.replace('\\','/')}'
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
 
